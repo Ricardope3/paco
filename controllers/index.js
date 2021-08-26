715dbf2f6f2ada78f845cff4340a25808fbfe6a6
Luis Ricardo Espinoza Rodriguez <ricardo.espinoza@ricardo.espinoza>
2021-08-25 20:19:15 -0500

    lmao

---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+const { getTaxiRecordsFromDB } = require('../datasources/index')
+
+const getTaxiRecords = async (req, res) => {
+    const data = getTaxiRecordsFromDB();
+    res.send(data);
+};
+
+const helloWorld = async (req, res) => {
+    res.send('Hello world');
+};
+
+module.exports = {
+    getTaxiRecords,
+};
